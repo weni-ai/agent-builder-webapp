9963e0635fc9fe8a1f41e32877532fbfabd3f70a
Aldemylla <aldemylla@gmail.com>
2025-12-16 11:04:23 -0300

    feat: Add AgentModalHeader component and refactor AgentDetailModal to use it

---
+++ src/components/AgentsTeam/AgentModalHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <UnnnicDialogHeader>
+    <AgentIcon
+      class="agent-modal-header__icon"
+      :icon="agent.icon"
+      data-testid="agent-icon"
+    />
+
+    <UnnnicDialogTitle>{{ agent.name }}</UnnnicDialogTitle>
+
+    <UnnnicTag
+      class="agent-card__tag"
+      :text="
+        agent.is_official
+          ? $t('router.agents_team.card.official')
+          : $t('router.agents_team.card.custom')
+      "
+      :scheme="agent.is_official ? 'teal' : 'purple'"
+      data-testid="agent-tag"
+    />
+  </UnnnicDialogHeader>
+</template>
+
+<script setup lang="ts">
+import AgentIcon from '@/components/AgentsTeam/AgentIcon.vue';
+import { AgentGroupOrAgent } from '@/store/types/Agents.types';
+
+defineProps<{
+  agent: AgentGroupOrAgent;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.agent-modal-header {
+  &__icon {
+    margin-right: $unnnic-space-2;
+    padding: 6px;
+
+    width: 48px;
+    height: auto;
+    aspect-ratio: 1/1;
+  }
+}
+</style>
