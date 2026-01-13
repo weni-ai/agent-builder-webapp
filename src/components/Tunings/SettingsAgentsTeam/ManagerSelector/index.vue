2284b8d67238295b0b3945eb54e18be78e1e83bb
Aldemylla <aldemylla@gmail.com>
2026-01-13 16:49:11 -0300

    feat: Refactor upgrade disclaimer into a separate component and update ManagerSelector to use it

--- src/components/Tunings/SettingsAgentsTeam/ManagerSelector/index.vue
+++ src/components/Tunings/SettingsAgentsTeam/ManagerSelector/index.vue
@@ -1,24 +1,6 @@
 <template>
   <section class="manager-selector">
-    <UnnnicDisclaimer
-      v-if="shouldShowUpgradeDisclaimer"
-      type="attention"
-      :title="
-        $t(
-          'agent_builder.tunings.manager.upgrade_banner.disclaimer_title_with_date',
-          {
-            manager_name: managers.new.label,
-            date: formattedLegacyDeprecation,
-          },
-        )
-      "
-      :description="
-        $t(
-          'agent_builder.tunings.manager.upgrade_banner.manager_2_dot_6_disclaimer_description',
-        )
-      "
-      data-testid="manager-upgrade-banner"
-    />
+    <UpgradeDisclaimer v-if="shouldShowUpgradeDisclaimer" />
 
     <ManagerUpgradeCard
       v-else-if="shouldUpgradeManager"
@@ -67,11 +49,10 @@
 import { computed } from 'vue';
 import { storeToRefs } from 'pinia';
 
-import ManagerUpgradeCard from './ManagerUpgradeCard.vue';
 import { useManagerSelectorStore } from '@/store/ManagerSelector';
-import { format } from 'date-fns';
-import { enUS, es as esLocale, ptBR } from 'date-fns/locale';
-import i18n from '@/utils/plugins/i18n';
+
+import ManagerUpgradeCard from './ManagerUpgradeCard.vue';
+import UpgradeDisclaimer from './UpgradeDisclaimer.vue';
 
 const managerSelectorStore = useManagerSelectorStore();
 const {
@@ -79,28 +60,9 @@ const {
   selectedManager,
   shouldUpgradeManager,
   shouldShowUpgradeDisclaimer,
-  legacyDeprecationDate,
 } = storeToRefs(managerSelectorStore);
 
-const localeFormats = {
-  en: { locale: enUS, pattern: 'MMMM d, yyyy' },
-  es: { locale: esLocale, pattern: "d 'de' MMMM 'de' yyyy" },
-  'pt-br': { locale: ptBR, pattern: "d 'de' MMMM 'de' yyyy" },
-};
-
 const managers = computed(() => options.value?.managers);
-const formattedLegacyDeprecation = computed(() => {
-  if (!legacyDeprecationDate.value) {
-    return '';
-  }
-
-  const normalizedLocale = i18n.global.locale?.toLowerCase() || 'en';
-  const localeConfig = localeFormats[normalizedLocale] || localeFormats.en;
-
-  return format(legacyDeprecationDate.value, localeConfig.pattern, {
-    locale: localeConfig.locale,
-  });
-});
 
 const updateSelectedManager = (managerId) => {
   managerSelectorStore.setSelectedManager(managerId);
