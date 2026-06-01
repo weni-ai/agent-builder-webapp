4718667cf2b305d5368803f39c8d9c4c4be223fb
Aldemylla <aldemylla@gmail.com>
2026-06-01 15:54:44 -0300

    feat: Add data-testid attributes to InstructionsHeaderActions and create unit tests for the component

--- src/components/Instructions/InstructionsHeaderActions.vue
+++ src/components/Instructions/InstructionsHeaderActions.vue
@@ -1,11 +1,13 @@
 <template>
   <UnnnicButton
+    data-testid="export-instructions-button"
     type="secondary"
     :text="$t('agents.instructions.export_instructions')"
     @click="exportInstructions"
   />
 
   <UnnnicButton
+    data-testid="new-instruction-button"
     type="primary"
     :text="$t('agents.instructions.new_instruction')"
     @click="newInstruction"
