1eb9e272441e69718cf503ba45d3203da1903281
Aldemylla <aldemylla@gmail.com>
2026-06-01 15:48:36 -0300

    feat: Add InstructionsHeaderActions component and update localization strings

---
+++ src/components/Instructions/InstructionsHeaderActions.vue
@@ -0,0 +1,23 @@
+<template>
+  <UnnnicButton
+    type="secondary"
+    :text="$t('agents.instructions.export_instructions')"
+    @click="exportInstructions"
+  />
+
+  <UnnnicButton
+    type="primary"
+    :text="$t('agents.instructions.new_instruction')"
+    @click="newInstruction"
+  />
+</template>
+
+<script setup lang="ts">
+function exportInstructions() {
+  // TODO: Implement export instructions
+}
+
+function newInstruction() {
+  // TODO: Implement new instruction drawer
+}
+</script>
