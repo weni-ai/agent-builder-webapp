b39820f867e5990f18b8c56627a9600f29dd05e5
Aldemylla <aldemylla@gmail.com>
2026-06-18 17:31:54 -0300

    feat: Add NoAnalysisPerformed component and localization for improvements analysis
    - Introduced a new NoAnalysisPerformed component to display a message when no analysis has been run.
    - Added corresponding localization entries in English, Spanish, Portuguese, and Romanian for the new component.
    - Updated the Supervisor Improvements view to conditionally render the NoAnalysisPerformed component based on analysis status.

---
+++ src/components/ConversationsImprovements/NoAnalysisPerformed.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="conversations-improvements__no-analysis-performed">
+    <header class="no-analysis-performed__header">
+      <h2 class="no-analysis-performed__title">
+        {{ $t('audit.improvements.no_analysis.title') }}
+      </h2>
+      <p class="no-analysis-performed__description">
+        {{ $t('audit.improvements.no_analysis.description') }}
+      </p>
+    </header>
+
+    <UnnnicButton
+      type="primary"
+      :text="$t('audit.improvements.no_analysis.run_analysis')"
+      @click="improvementsStore.runAnalysis"
+    />
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useImprovementsStore } from '@/store/Improvements';
+
+const improvementsStore = useImprovementsStore();
+</script>
+
+<style scoped lang="scss">
+.conversations-improvements__no-analysis-performed {
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: $unnnic-space-4;
+}
+
+.no-analysis-performed {
+  &__header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: $unnnic-space-1;
+
+    text-align: center;
+  }
+
+  &__title {
+    font: $unnnic-font-action;
+    color: $unnnic-color-fg-emphasized;
+  }
+
+  &__description {
+    font: $unnnic-font-body;
+    color: $unnnic-color-fg-base;
+  }
+}
+</style>
