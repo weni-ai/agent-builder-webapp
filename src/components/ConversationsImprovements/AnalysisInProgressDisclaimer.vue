74e2ffb1b050e1054b288727a1384e1ae15691a3
Aldemylla <aldemylla@gmail.com>
2026-06-19 18:17:17 -0300

    feat: Add components for displaying analysis progress and improve localization
    - Introduced `AnalysisInProgressDisclaimer`, `ImprovementsHeader`, and `ImprovementsList` components to enhance the user interface during ongoing analysis.
    - Updated localization files to include messages for the analysis in progress disclaimer in English, Spanish, Portuguese, and Romanian.
    - Enhanced the Supervisor Improvements view to conditionally render the new components based on the analysis task state.

---
+++ src/components/ConversationsImprovements/AnalysisInProgressDisclaimer.vue
@@ -0,0 +1,27 @@
+<template>
+  <UnnnicDisclaimer
+    v-if="analysis.task?.isRunning"
+    data-testid="analysis-in-progress-disclaimer"
+    type="informational"
+    :description="
+      $t('audit.improvements.analysis_in_progress_disclaimer', {
+        progress: analysis.task?.progress ?? 0,
+        total: analysis.task?.total ?? 0,
+      })
+    "
+  >
+    <template #icon>
+      <UnnnicIconLoading
+        size="sm"
+        stroke-width="3"
+      />
+    </template>
+  </UnnnicDisclaimer>
+</template>
+<script setup lang="ts">
+import { useImprovementsStore } from '@/store/Improvements';
+import { storeToRefs } from 'pinia';
+
+const improvementsStore = useImprovementsStore();
+const { analysis } = storeToRefs(improvementsStore);
+</script>
