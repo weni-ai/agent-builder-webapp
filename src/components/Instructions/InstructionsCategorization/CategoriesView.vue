f75d57571a3422208ea970e1bc5436bc543656ec
Aldemylla <aldemylla@gmail.com>
2026-08-20 11:31:46 -0300

    feat: Add rename category functionality with modal support

--- src/components/Instructions/InstructionsCategorization/CategoriesView.vue
+++ src/components/Instructions/InstructionsCategorization/CategoriesView.vue
@@ -6,11 +6,13 @@
     <CategoryAccordion
       v-for="(group, index) in groups"
       :key="group.key"
+      :ref="(el) => setGroupRef(group.key, el)"
       :group="group"
       :initiallyExpanded="index === 0"
       :forceExpanded="instructionsStore.isSearching"
       :data-testid="`categories-view-group-${group.key}`"
       @delete-category="$emit('delete-category', $event)"
+      @rename-category="$emit('rename-category', $event)"
       @edit="$emit('edit', $event)"
     />
 
@@ -19,7 +21,7 @@
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, nextTick, watch } from 'vue';
 
 import { useFeatureFlagsStore } from '@/store/FeatureFlags';
 import { useInstructionsStore } from '@/store/Instructions';
@@ -27,13 +29,44 @@ import { useInstructionsStore } from '@/store/Instructions';
 import CategoryAccordion from './CategoryAccordion.vue';
 import SafetyGuardrails from './SafetyGuardrails.vue';
 
-defineEmits(['delete-category', 'edit']);
+const props = defineProps({
+  scrollToGroupKey: {
+    type: String,
+    default: null,
+  },
+});
+
+defineEmits(['delete-category', 'edit', 'rename-category']);
 
 const featureFlagsStore = useFeatureFlagsStore();
 const instructionsStore = useInstructionsStore();
 
 const featureFlags = computed(() => featureFlagsStore.flags);
 const groups = computed(() => instructionsStore.groupedInstructions);
+
+const groupRefs = {};
+
+function setGroupRef(key, el) {
+  if (el) {
+    groupRefs[key] = el;
+  } else {
+    delete groupRefs[key];
+  }
+}
+
+watch(
+  () => props.scrollToGroupKey,
+  (key) => {
+    if (!key) return;
+
+    nextTick(() => {
+      groupRefs[key]?.$el?.scrollIntoView?.({
+        block: 'nearest',
+        behavior: 'smooth',
+      });
+    });
+  },
+);
 </script>
 
 <style lang="scss" scoped>
